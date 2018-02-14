ea986d5c962102125cec53160b9f63e474dd25ad
Jeff McFadden <jeff@mcfadden.io>
2018-02-14 13:16:02 -0700

    API for adding sensor observations.

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,12 +1,15 @@
+class InvalidAPITokenError < StandardError 
+end
+  
 class Api::ApplicationController < ApplicationController
   protect_from_forgery with: :null_session
-  
-  class InvalidAPITokenError < StandardError;
   
   private
   
   def verify_api_token
-    raise InvalidAPITokenError, "API Token Invalid" unless request.headers["Authorization"] == "Bearer #{ENV['API_TOKEN']}"
+    unless request.headers["Authorization"] == "Bearer #{ENV['API_TOKEN']}"
+      raise InvalidAPITokenError, "API Token Invalid" 
+    end
   end
   
 end
--- app/controllers/api/sensor_observations_controller.rb
+++ app/controllers/api/sensor_observations_controller.rb
@@ -3,8 +3,8 @@ class Api::SensorObservationsController < Api::ApplicationController
   before_action :verify_api_token, only: [:create]
   
   def create
-    observation = SensorObservation.find_or_create_by( sensor_id: params[:sensor_id], observed_at: params[:observed_at] )
-    observation.value = params[:value]
+    observation = create_or_find_sensor_observation
+    observation.value = params[:sensor_observation][:value]
     observation.save
     
     respond_to do |format|
@@ -14,6 +14,10 @@ class Api::SensorObservationsController < Api::ApplicationController
   end
   
   private
+  
+  def create_or_find_sensor_observation
+    SensorObservation.find_or_create_by( sensor_id: params[:sensor_observation][:sensor_id], observed_at: params[:sensor_observation][:observed_at] )
+  end
   
   def sensor_observation_params
     params.require( :sensor_observation ).permit( :sensor_id, :observed_at, :value )
