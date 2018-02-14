a5aa80cdb0993fe4700d6503a1ee8bd47e3e26a9
Jeff McFadden <jeff@mcfadden.io>
2018-02-14 10:52:29 -0700

    Deploy prep

--- README.md
+++ README.md
@@ -1,24 +1,36 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+Weather Station
 
-Things you may want to cover:
+## Setting up my RPi
 
-* Ruby version
+    sudo apt-get update
+    sudo apt-get upgrade
+    sudo apt-get install gcc g++ make bison libyaml-dev libssl-dev libffi-dev zlib1g-dev libxslt-dev libxml2-dev libpq-dev zip nodejs vim libreadline-dev postgresql postgresql-contrib libpq-dev nginx wget
+    
+    mkdir installs
+    cd installs
+    wget "https://cache.ruby-lang.org/pub/ruby/2.4/ruby-2.4.3.tar.gz"
+    tar -zxvf ruby-2.4.3.tar.gz
+    cd ruby-2.4.3
+    ./configure --disable-install-rdoc
+    make -j4
+    sudo make install
+    sudo gem install bundler --no-document
+    
+    sudo mkdir /www
+    sudo chown pi:pi /www
 
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+    # On my local box:
+    # ssh-copy-id -i ~/.ssh/mykey user@host
+    
+    sudo -i -u postgres
+    > createuser -s -P rails
+    > exit
+    
+    sudo vim /etc/postgresql/9.6/main/pg_hba.conf
+    
+    # Change the line below this comment from 'peer' to 'md5':
+    # # "local" is for Unix domain socket connections only
+    
+    sudo service postgresql restart
---
+++ config/deploy.rb
@@ -0,0 +1,104 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.10.1"
+
+set :application, "weather_station"
+set :repo_url, "git@example.com:jeffmcfadden/weather_station.git"
+
+# Default branch is :master
+set :branch, ENV.fetch('REVISION', 'master')
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/www/weather_station'
+
+# Default value for :linked_files is []
+set :linked_files, fetch(:linked_files, []).push('config/secrets.yml', 'config/application.yml')
+
+# Default value for linked_dirs is []
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# set the locations that we will look for changed assets to determine whether to precompile
+set :assets_dependencies, %w(app/assets lib/assets vendor/assets Gemfile.lock config/routes.rb)
+
+
+# clear the previous precompile task
+Rake::Task["deploy:assets:precompile"].clear_actions
+class PrecompileRequired < StandardError; end
+
+# Default value for keep_releases is 5
+set :keep_releases, 5
+
+namespace :deploy do
+
+  namespace :services do
+    desc "Restart All Services"
+    task :restart do
+      on roles(:web) do
+        within release_path do
+          with rails_env: fetch(:rails_env) do
+            # on OS X the equivalent pid-finding command is `ps | grep '/puma' | head -n 1 | awk {'print $1'}`
+            
+            # This tells puma to kick off a phased restart, and falls back to a normal hard restart or start if necessary.
+            #execute "(sudo kill -s SIGUSR2 $(ps -C ruby -F | grep '/puma' | awk {'print $2'}))"
+            # execute "sudo pumactl --config-file /www/dwell/current/config/puma.rb phased-restart"
+            # execute "sudo pumactl --config-file /www/dwell/current/config/puma.rb phased-restart"
+            execute "sudo systemctl restart puma.service"
+            
+            #" || sudo service puma restart || sudo service puma start"
+          end
+        end
+      end
+      # on roles(:background) do
+      #   within release_path do
+      #     with rails_env: fetch(:rails_env) do
+      #       execute "sudo systemctl restart sidekiq"
+      #       # execute "sudo restart sidekiq-media"
+      #       # execute "sudo restart clockwork"
+      #     end
+      #   end
+      # end
+    end
+  end
+
+  namespace :assets do
+    desc "Precompile assets"
+    task :precompile do
+      on roles(fetch(:assets_roles)) do
+        within release_path do
+          with rails_env: fetch(:rails_env) do
+            begin
+              # find the most recent release
+              latest_release = capture(:ls, '-xr', releases_path).split[1]
+
+              # precompile if this is the first deploy
+              raise PrecompileRequired unless latest_release
+
+              latest_release_path = releases_path.join(latest_release)
+
+              # precompile if the previous deploy failed to finish precompiling
+              execute(:ls, latest_release_path.join('assets_manifest_backup')) rescue raise(PrecompileRequired)
+
+              fetch(:assets_dependencies).each do |dep|
+                # execute raises if there is a diff
+                execute(:diff, '-Naur', release_path.join(dep), latest_release_path.join(dep)) rescue raise(PrecompileRequired)
+              end
+
+              info("Skipping asset precompile, no asset diff found")
+
+              # copy over all of the assets from the last release
+              execute(:cp, '-r', latest_release_path.join('public', fetch(:assets_prefix)), release_path.join('public', fetch(:assets_prefix)))
+            rescue PrecompileRequired
+              execute(:rake, "assets:precompile")
+            end
+          end
+        end
+      end
+    end
+  end
+
+end
+
+after  "deploy:published", "deploy:services:restart"
+# after "deploy:updated", "newrelic:notice_deployment"
