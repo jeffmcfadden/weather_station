9afa11e39dccda46b6ecc3009bc550be98c163b9
Jeff McFadden <jeff@mcfadden.io>
2018-02-15 16:30:57 -0700

    Layout homepage a bit better.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def formatted_temperature(t)
+    f = (t * (9.0/5.0) + 32.0).round(1)
+    "#{f}°F"
+  end
+
 end
--- app/models/sensor.rb
+++ app/models/sensor.rb
@@ -3,4 +3,8 @@ class Sensor < ApplicationRecord
 
   enum sensor_type: { temperature: 0, relative_humitity: 1, barometric_pressure: 2 }
 
+  def latest_observation
+    sensor_observations.order( observed_at: :desc ).limit(1).first
+  end
+
 end
