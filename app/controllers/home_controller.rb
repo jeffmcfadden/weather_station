4865f6a364ea68cb4f0e7382fad45a29ad7cb1fc
Jeff McFadden <jeff@mcfadden.io>
2018-02-16 15:18:34 -0700

    Change data on front page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -15,11 +15,11 @@ class HomeController < ApplicationController
   def recent_highs_and_lows_for_chart
     
     @recent_high_temperatures = Rails.cache.fetch("v1/recent_high_temperatures", expires_in: 5.minutes ) do
-      SensorDailyAggregation.where( sensor_id: 1 ).where( "day > ?", 90.days.ago ).collect{ |a| [a.day, a.maximum.to_f * (9.0/5.0) + 32.0] }
+      SensorDailyAggregation.where( sensor_id: 1 ).where( "day > ?", 45.days.ago ).collect{ |a| [a.day, a.maximum.to_f * (9.0/5.0) + 32.0] }
     end
 
     @recent_low_temperatures  = Rails.cache.fetch("v1/recent_low_temperatures", expires_in: 5.minutes ) do
-      SensorDailyAggregation.where( sensor_id: 1 ).where( "day > ?", 90.days.ago ).collect{ |a| [a.day, a.minimum.to_f * (9.0/5.0) + 32.0] }
+      SensorDailyAggregation.where( sensor_id: 1 ).where( "day > ?", 45.days.ago ).collect{ |a| [a.day, a.minimum.to_f * (9.0/5.0) + 32.0] }
     end
     
     # [{name: 'High Temperature', data: @recent_high_temperatures, library: {lineTension: 0.25, pointRadius: 0, responsive: true, scales: { yAxes: [{ ticks:{ stepSize: 10 } }] }}}, {name: 'Low Temperature', data: @recent_low_temperatures, library: {lineTension: 0.25, pointRadius: 0}}]
