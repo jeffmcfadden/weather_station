e8bbbb3151792eb29e25f3f40bd3b4413600b1fc
Jeff McFadden <jeff@mcfadden.io>
2018-02-18 14:51:48 -0700

    Show the vertical gridlines on these graphs.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,12 +4,12 @@ class HomeController < ApplicationController
   
   def recent_temperatures_for_chart
     @recent_temperatures = Rails.cache.fetch("v1/recent_temperatures", expires_in: 5.minutes) do
-      SensorObservation.where( sensor_id: 1 ).where( "observed_at > ?", Time.now.in_time_zone - 3.days ).collect{ |so| [so.observed_at, (so.value * (9.0/5.0) + 32.0).round(1)] }
+      SensorObservation.where( sensor_id: 1 ).where( "observed_at > ?", Time.now.in_time_zone - 2.days ).collect{ |so| [so.observed_at, (so.value * (9.0/5.0) + 32.0).round(1)] }
     end
     
     # [{name: 'Temperature', data: @recent_temperatures, min: nil, max: nil, library: {borderWidth: 1, lineTension: 0.05, pointRadius: 0}}]
     
-    render json: [{name: 'Temperature', data: @recent_temperatures, min: nil, max: nil, library: {borderWidth: 1, lineTension: 0.05, pointRadius: 0}}]
+    render json: [{name: 'Temperature', data: @recent_temperatures, min: nil, max: nil, library: {borderWidth: 1, lineTension: 0.05, pointRadius: 0, scales: { xAxes: [{gridLines: { display: true }}]}}}]
   end
   
   def recent_highs_and_lows_for_chart
