640d5b16a81930eedfd4259e982b7d8274581e40
Jeff McFadden <jeff@mcfadden.io>
2018-02-18 14:53:41 -0700

    Try tweaking the start point to always be midnight.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   
   def recent_temperatures_for_chart
     @recent_temperatures = Rails.cache.fetch("v1/recent_temperatures", expires_in: 5.minutes) do
-      SensorObservation.where( sensor_id: 1 ).where( "observed_at > ?", Time.now.in_time_zone - 2.days ).collect{ |so| [so.observed_at, (so.value * (9.0/5.0) + 32.0).round(1)] }
+      SensorObservation.where( sensor_id: 1 ).where( "observed_at > ?", (Time.now.in_time_zone - 2.days).beginning_of_day ).collect{ |so| [so.observed_at, (so.value * (9.0/5.0) + 32.0).round(1)] }
     end
     
     # [{name: 'Temperature', data: @recent_temperatures, min: nil, max: nil, library: {borderWidth: 1, lineTension: 0.05, pointRadius: 0}}]
