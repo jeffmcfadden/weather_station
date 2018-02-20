3e8d81a8ebcb294b70a3782c431394ef7ba7b003
Jeff McFadden <jeff@mcfadden.io>
2018-02-20 09:34:25 -0700

    Add another legacy handler.

--- app/controllers/api/legacy_controller.rb
+++ app/controllers/api/legacy_controller.rb
@@ -8,5 +8,17 @@ class Api::LegacyController < Api::ApplicationController
       format.json { render json: { sensor: @sensor } }
     end
   end
+  
+  def latest
+    @latest_values = {}
+    
+    Sensor.all.each do |s|
+      @latest_values[s.name] = s.latest_value
+    end
+    
+    respond_to do |format|
+      format.json { render json: @latest_values }
+    end
+  end
 
 end
